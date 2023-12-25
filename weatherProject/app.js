2c6fe9c2a43e59170bbd5c43a6d97f2aeb8ee12c
Suraj Mandal <rjsurajmandal@gmail.com>
2023-12-25 16:17:41 +0530

    Refactor weather app UI and improve user experience

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -1,29 +1,82 @@
-const loading = document.querySelector(".loading-container");
 const permission = document.querySelector(".grant-permission-container");
+const loading = document.querySelector(".loading-container");
+const network = document.querySelector(".network-container");
+const search = document.querySelector(".search-container");
+const weather = document.querySelector(".weather-container");
+const error = document.querySelector(".error-container");
+
+const locationName = document.querySelector(".location-name");
+const weatherType = document.querySelector(".weather-condition p");
+const temprature = document.querySelector(".temp");
+const windspeedVal = document.querySelector(".windspeed-value");
+const humidityVal = document.querySelector(".humidity-value");
+const cloudsVal = document.querySelector(".clouds-value");
+const locationImg = document.querySelector(".user-location img");
+const weatherImg = document.querySelector(".weather-condition img");
+
+const weatherTab = document.querySelector(".weather-tab");
+const searchTab = document.querySelector(".search-tab")
+
+
 const permissionMsg = document.querySelector(".grant-permission-container p");
 const grantBtn = document.querySelector(".grant-btn");
 
+let tempData;
 
 
 const API_kEY = "d1845658f92b31c64bd94f06f7188c9c";
 let flag = true; //tracking if first time user clicked allow or block
 
-
-function startLoading() {
+//these function hiding and showing UI components 
+function showLoadingContainer() {
     loading.classList.add("active-block");
 }
 
-function removeLoading() {
+function hideLoadingContainer() {
     loading.classList.remove("active-block");
 }
 
-function showPermissionPage(){
+function showPermissionContainer(){
     permission.classList.add("active-flex")
 }
 
-function hidePermissionPage(){
+function hidePermissionContainer(){
     permission.classList.remove("active-flex");
 }
+
+function showWeatherContainer(){
+    weather.classList.add("active-block");
+}
+
+function hideWeatherContainer(){
+    weather.classList.remove("active-block");
+}
+
+function showNetworkContainer(){
+    network.classList.add("active-block");
+}
+
+function hideNetworkContainer(){    
+    network.classList.remove("active-block");
+}
+
+function showErrorContainer(){
+    error.classList.add("active-block");
+}
+
+function hideErrorContainer(){  
+    error.classList.remove("active-block");
+}
+
+function showSearchContainer(){
+    search.classList.add("active-flex");
+}
+
+function hideSearchContainer(){
+    search.classList.remove("active-flex");
+}
+
+
 
 async function fetchUserLocation(latitude, longitude) {
 
@@ -38,20 +91,19 @@ async function fetchUserLocation(latitude, longitude) {
 
 async function fetchWeatherDetails(city) {
 
-    startLoading();
+    showLoadingContainer();
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_kEY}&units=metric`);
     let data = await response.json();
 
-    removeLoading();
-    console.log(data);
+    renderWhetherInfo(data);
 }
 
 function getUserLocation() {
 
     //if user didn't block the request first time
     if(flag){
-        hidePermissionPage();
-        startLoading();
+        hidePermissionContainer();
+        showLoadingContainer();
         navigator.geolocation.getCurrentPosition(
             function (position){
                 if (navigator.geolocation) {
@@ -64,7 +116,7 @@ function getUserLocation() {
     
             function (e){
                 flag = false;  //user selected block 
-                removeLoading();
+                hideLoadingContainer();
                 switch (e.code) {
                     case e.PERMISSION_DENIED:
                         permissionMsg.textContent = "you denied the request for Geolocation.";
@@ -76,7 +128,7 @@ function getUserLocation() {
                         permissionMsg.textContent = "The request to get user location timed out.";
                         break;
                 }
-                showPermissionPage();
+                showPermissionContainer();
             }
         
         ); 
@@ -85,13 +137,60 @@ function getUserLocation() {
 
 
 
+function renderWhetherInfo(weatherData){
 
-grantBtn.addEventListener("click",getUserLocation)
+
+    locationName.textContent = weatherData.name;
+
+    weatherType.textContent = weatherData["weather"][0]["main"];
+
+    temprature.textContent = `${weatherData["main"]["temp"]}°C`;
+
+    windspeedVal.textContent = `${weatherData["wind"]["speed"]}m/s`;
+
+    humidityVal.textContent = `${weatherData["main"]["humidity"]}%`;
+
+    cloudsVal.textContent = `${weatherData["clouds"]["all"]}%`;
+
+    let cityName = weatherData["sys"]["country"];
+    cityName = cityName.toLowerCase();
+    
+    locationImg.src =`https://flagcdn.com/144x108/${cityName}.png` ;
+
+    weatherImg.src = `https://openweathermap.org/img/w/${weatherData["weather"][0]["icon"]}.png`;
+
+    hideLoadingContainer();
+    showWeatherContainer();
+}
+
+
+
+// main program starts here
+if(!localStorage.getItem("city"))
+    showPermissionContainer();
+
 window.addEventListener("load",()=>{
     if(localStorage.getItem("city")){
-        hidePermissionPage();
-        startLoading();
-        getUserLocation();
+        fetchWeatherDetails(localStorage.getItem("city"));
     }
 })
 
+
+grantBtn.addEventListener("click",getUserLocation)
+searchTab.addEventListener("click",()=>{
+    if(!search.classList.contains("active-flex")){
+        searchTab.classList.add("current-tab");
+        weatherTab.classList.remove("current-tab");
+        hideWeatherContainer();
+        showSearchContainer();
+    }
+})
+
+weatherTab.addEventListener("click",()=>{
+    if(search.classList.contains("active-flex")){
+        searchTab.classList.remove("current-tab");
+        weatherTab.classList.add("current-tab");
+        hideSearchContainer();
+        fetchWeatherDetails(localStorage.getItem("city"))
+    }
+})
